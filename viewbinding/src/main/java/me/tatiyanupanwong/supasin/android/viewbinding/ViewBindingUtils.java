5b85673abc4086230c8e42bbb433da1221ea57ed
Supasin Tatiyanupanwong <supasin@tatiyanupanwong.me>
2018-08-10 16:18:56 +0700

    Prevented searching in framework classes

--- viewbinding/src/main/java/me/tatiyanupanwong/supasin/android/viewbinding/ViewBindingUtils.java
+++ viewbinding/src/main/java/me/tatiyanupanwong/supasin/android/viewbinding/ViewBindingUtils.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2015-2018 Supasin Tatiyanupanwong
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package me.tatiyanupanwong.supasin.android.viewbinding;
 
 import android.app.Activity;
@@ -56,6 +72,10 @@ public final class ViewBindingUtils {
 
         Class<?> clazz = target.getClass();
         while (clazz != null) {
+            String clazzName = clazz.getName();
+            if (clazzName.startsWith("android.") || clazzName.startsWith("java.")) {
+                break; // Reached framework class. Abandoning search.
+            }
 
             for (Field field : clazz.getDeclaredFields()) {
                 BindView annotation = field.getAnnotation(BindView.class);
